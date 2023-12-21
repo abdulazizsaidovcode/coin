e74da2adcc1ef3b17f9618655ea5798af797d020
Behruz1227 <behruzxonniyozov12@gmail.com>
2023-12-21 16:19:35 +0500

    text red

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css"
 
 function App() {
   return (
-    <div className="text-white">
+    <div className="text-red-900">
       Hello3ab38d13ec5e12f9841029c3e1b0a435483b
     </div>
   )
