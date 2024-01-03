fe2d504917b0b7e905f7b38e880270bd922c9393
AbdulvohidAbdusaidov <yanzinipubg@gmail.com>
2024-01-03 15:36:16 +0500

    hello

--- src/components/studentdashboard/index.jsx
+++ src/components/studentdashboard/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { config, url } from "../api/api";
+import { config, setConfig, url } from "../api/api";
 
 function StudentDashboard() {
     const [name, setName] = useState("");
 
     useEffect(() => {
+        setConfig()
         axios.get(url + "user/getMe", config)
             .then(response => {
                 setName(response.data.body.fullName);
@@ -24,10 +25,8 @@ function StudentDashboard() {
             <div className="bg-white rounded-lg p-4 flex flex-col sm:flex-row justify-between items-center shadow-md">
                 <div className="flex flex-col items-center mb-4 sm:mb-0">
                     <i className="fa-solid fa-star text-yellow-500 text-2xl"></i>
-                    <div>
-                        <span className="text-lg font-semibold">3255</span>
-                        <span className="text-sm text-gray-600">Total coin</span>
-                    </div>
+                    <span className="text-lg font-semibold">3255</span>
+                    <span className="text-sm text-gray-600">Total coin</span>
                 </div>
                 <div className="flex flex-col items-center mb-4 sm:mb-0">
                     <i className="fa-solid fa-trophy text-yellow-300 text-2xl"></i>
