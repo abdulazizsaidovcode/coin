ce817d314e20a4842c140b8746879047498b654b
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-05 17:16:19 +0500

    oooooo

--- src/teacher/components/dashboard/Dashboard.jsx
+++ src/teacher/components/dashboard/Dashboard.jsx
@@ -14,6 +14,7 @@ const Dashboard = () => {
     const [topStudent, setTopStudent] = useState(null);
     const [topGroup, setTopGroup] = useState(null);
     const [pl, setPl] = useState(null);
+    const [name, setName] = useState([]);
 
     useEffect(() => {
         setConfig();
@@ -38,7 +39,6 @@ const Dashboard = () => {
             .then((res) => setPl(res.data.body))
             .catch((err) => console.log(err));
     }, []);
-    const [name, setName] = useState("");
 
     useEffect(() => {
         axios
@@ -50,6 +50,8 @@ const Dashboard = () => {
                 console.log("Boshqa backendinchi topiyla iltimos 😭", error);
             });
     }, []);
+
+    console.log(name);
 
     return (
         <>
