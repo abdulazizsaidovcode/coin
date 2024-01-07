fb8a38b4f085ed1562977c14c578d8479d20db60
Ozodbek <ozodbek007@gmail.com>
2024-01-07 10:07:58 +0500

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Teacher from "./teacher/index"
 import Student from "./student/index"
 import {byId} from "./components/api/api";
 import {useEffect} from "react";
+import axios from "axios";
 
 function App() {
 
@@ -14,6 +15,22 @@ function App() {
         if (sessionStorage.getItem('role') === 'ROLE_TEACHER') return <Teacher/>
         if (sessionStorage.getItem('role') === 'ROLE_USER') return <Student/>
     }
+
+    const config = {
+        headers: {
+            Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS5tb2RtZS51ei92MS9hdXRoL2xvZ2luIiwiaWF0IjoxNzA0NTIyOTEzLCJleHAiOjE3MDQ1NjYxMTMsIm5iZiI6MTcwNDUyMjkxMywianRpIjoiMmZBbWZicWNaWW93OE1BWCIsInN1YiI6IjY0OTcwOCIsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.LlkMwcTaYRkdb4uAWngne_jDyF0pQJ4kHWfHpNihJsU',
+        }
+    }
+
+    useEffect(() => {
+        axios.get('https://api.modme.uz/v1/leadSection?branch_id=3866&sections_id[]=113068&sections_id[]=129868&sections_id[]=113067&sections_id[]=113066&sections_id[]=113065&sections_id[]=113064&sections_id[]=127219&sections_id[]=123096&sections_id[]=113077&sections_id[]=113076', config)
+            .then(res => {
+                console.log(res);
+            })
+        fetch('https://api.modme.uz/v1/leadSection?branch_id=3866&sections_id[]=113068&sections_id[]=129868&sections_id[]=113067&sections_id[]=113066&sections_id[]=113065&sections_id[]=113064&sections_id[]=127219&sections_id[]=123096&sections_id[]=113077&sections_id[]=113076', {
+
+        })
+    }, []);
 
     return (
         <div className="flex">
