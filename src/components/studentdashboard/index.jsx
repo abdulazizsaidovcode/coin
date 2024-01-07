d96acac6e5e329ba2e5575488163fae07a539050
Ozodbek <ozodbek007@gmail.com>
2024-01-07 15:08:53 +0500

    student Dashboard top Group and top Student

--- src/components/studentdashboard/index.jsx
+++ src/components/studentdashboard/index.jsx
@@ -3,22 +3,25 @@ import StudentsTotalcoin from "./Totalcoins";
 import Studetsrate from "./Studentsrate/index.jsx";
 import Studentcoinstatistic from "./Studentcoinstatistic/index.jsx";
 import TopGroup from "../TopGroups/index.jsx";
-import {getTopGroup, getTopStudent, getTopTeacher} from "../api/api";
+import {getStudentStatistics, getTopGroupForTeacher, getTopStudent, setConfig} from "../api/api";
 import TopLoading from "../Topteachers/TopLoading";
-import TopTeachers from "../Topteachers";
 import TopStudent from "../Topstudents";
 
 function StudentDashboard() {
 
     const [topGroup, setTopGroup] = useState(null);
     const [topStudent, setTopStudent] = useState(null);
+    const [studentStatistics, setStudentStatistics] = useState(null);
 
 
     useEffect(() => {
-        getTopGroup(setTopGroup);
+        setConfig();
+        getTopGroupForTeacher(setTopGroup);
         getTopStudent(setTopStudent);
+        getStudentStatistics(setStudentStatistics);
     }, []);
 
+    console.log(studentStatistics)
     return (
         <div className="bg-gray-100 min-h-screen p-4 sm:p-8  w-full">
             <div className="w-full py-5">
@@ -26,7 +29,11 @@ function StudentDashboard() {
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-3 w-full ">
                 <div className="col-span-1 lg:col-span-6 mb-3">
-                    <StudentsTotalcoin/>
+                    {studentStatistics ? (
+                        <StudentsTotalcoin studentStatistics={studentStatistics}/>
+                    ) : (
+                        <StudentsTotalcoin studentStatistics={{coin: 0, rate: 0, time: 0, completedCourse: 0}}/>
+                    )}
                 </div>
                 <div className="col-span-1 lg:col-span-6 ">
                     <Studentcoinstatistic/>
