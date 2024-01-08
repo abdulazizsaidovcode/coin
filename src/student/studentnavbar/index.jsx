42aaa5b0be5b3c6be5728bdc66d1ac3efe8dca92
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-09 01:38:33 +0500

    aaaaaaaaaaaaaa
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/student/studentnavbar/index.jsx
+++ src/student/studentnavbar/index.jsx
@@ -1,39 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { config, url } from "../../components/api/api.js";
+import { config, setConfig, url } from "../../components/api/api.js";
 import opacha from "../../assits/opacha.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faCheckCircle, faClock, faDonate, faDotCircle } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faCheckCircle, } from '@fortawesome/free-solid-svg-icons';
+import "./index.css";
 
 const StudentNavbar = () => {
     // Foydalanuvchi ma'lumotlari uchun alohida state o'zgaruvchilari
-    const [fullName, setFullName] = useState('');
-    const [attachment, setAttachment] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [email, setGroupName] = useState('');
-    const [coin, setCoin] = useState('');
-
-    // useEffect orqali komponent yuklanganda bir martalik backenddan ma'lumot olish
-    useEffect(() => {
-        axios.get(url + "user/getMe", config)
-            .then(response => {
-                // Foydalanuvchi ma'lumotlarini alohida state o'zgaruvchilariga joylaymiz
-                const userData = response.data; // .body olib tashlandi chunki bu struktura backendga bog'liq
-                setFullName(userData.body.fullName);
-                setAttachment(userData.body.attachment);
-                setPhoneNumber(userData.body.phoneNumber);
-                setGroupName(userData.body.email);
-                setCoin(userData.body.coin);
-            })
-            .catch(error => {
-                console.error("Backenddan ma'lumot olishda xatolik yuz berdi", error);
-            });
-    }, []);
+    const [name, setName] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        setConfig();
+        axios.get(url + "user/getMe", config)
+            .then(res => {
+                setName(res.data.body);
+            })
+            .catch(err => console.log("Boshqa backendinchi topiyla iltomos 😭", err));
+    }, []);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
+
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        setConfig();
+        axios.get(url + "message/student", config)
+            .then(res => {
+                if (res.data && res.data.body && res.data.body.object) {
+                    setMessages(res.data.body.object);
+                }
+            })
+            .catch(err => console.log("Backenddan ma'lumot olishda xatolik yuz berdi 😭", err));
+    }, []);
+
 
     return (
         <div className="bg-gray-100 w-full">
@@ -49,11 +52,16 @@ const StudentNavbar = () => {
                     <button onClick={toggleMenu} className="flex items-center space-x-2 ">
                         <FontAwesomeIcon icon={faCheckCircle} className="text-2xl mr-2 text-gray-800" />
                         <div className='relative mt-1'>
-                            <div className='w-2 h-2 bg-red-400 rounded-full absolute right-2 '><a href=""></a></div>
-                            <FontAwesomeIcon icon={faBell} className="text-2xl mr-2 text-gray-800" />
+                            {messages.length > 0 ? (
+                                <div className='w-2 h-2 bg-red-400 rounded-full absolute right-2 '><a href=""></a></div>
+                            ) : (
+                                <div></div>
+                            )}
+
+                            <FontAwesomeIcon icon={faBell} className={`${messages.length > 0} ` ? "text-2xl mr-2 text-gray-80 anim" : "text-2xl mr-2 text-gray-80"} />
                         </div>
                         <img src={opacha} alt="Admin" className="rounded-full border p-1 w-12 h-12" />
-                        <span className="hidden md:block">{fullName}</span>
+                        <span className="hidden md:block">{name.fullName}</span>
                     </button>
                     <div
                         className={`${isOpen ? "absolute" : " hidden"
@@ -65,10 +73,10 @@ const StudentNavbar = () => {
                         </div>
                         <div className="px-6 py-4">
                             <div className="font-bold text-xl mb-2 text-center"></div>
-                            <p className="text-gray-700 text-base text-center">{email}</p>
-                            <p className="text-gray-700 text-base text-center">{fullName}</p>
-                            <p className="text-gray-700 text-base text-center">{phoneNumber}</p>
-                            <p className="text-gray-700 text-base text-center">{coin}</p>
+                            <p className="text-gray-700 text-base text-center">{name.email}</p>
+                            <p className="text-gray-700 text-base text-center">{name.fullName}</p>
+                            <p className="text-gray-700 text-base text-center">{name.phoneNumber}</p>
+                            <p className="text-gray-700 text-base text-center">{name.coin}</p>
                         </div>
                         <div className="px-6 pt-4 text-center flex justify-between">
                             <button className='btm'>edit</button>
@@ -79,7 +87,7 @@ const StudentNavbar = () => {
             </div>
             <div className="px-8 pt-10">
                 <h1 className="text-3xl sm:text-4xl font-semibold text-gray-800">
-                    Hi {fullName}
+                    Hi {name.fullName}
                 </h1>
                 <span className="text-sm text-gray-600">
                     Welcome back to the Coin system dashboard
