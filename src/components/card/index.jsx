ddf5ad321b6fe8697ecafca04bd3e03f013514f8
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-01-08 22:25:03 +0500

    jj

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -161,7 +161,7 @@ const GiftCard = ({ gifts, getGift }) => {
                   </label>
                   <input
                     type="text"
-                    name="name"
+                    defaultValue={giftIn.name}
                     id="name"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Type product name"
@@ -177,11 +177,10 @@ const GiftCard = ({ gifts, getGift }) => {
                   </label>
                   <input
                     type="number"
-                    name="rate"
+                    defaultValue={giftIn.rate}
                     id="rate"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="100"
-                    required=""
                   />
                 </div>
                 <div className="col-span-2 sm:col-span-1">
@@ -204,6 +203,7 @@ const GiftCard = ({ gifts, getGift }) => {
                   <textarea
                     id="description"
                     rows="4"
+                    defaultValue={giftIn.description}
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Write product description here"
                   ></textarea>
