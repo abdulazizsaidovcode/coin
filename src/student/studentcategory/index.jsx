d2c6fface8858dee1e09713e294feed7ccdbde9b
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-02-02 11:50:55 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/student/studentcategory/index.jsx
+++ src/student/studentcategory/index.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { config, setConfig, url } from '../../components/api/api';
-import axios from 'axios';
-import Studentstatistic from './statisctic';
+import React, { useEffect, useState } from "react";
+import { config, setConfig, url } from "../../components/api/api";
+import axios from "axios";
+import Studentcoinstatistic from "../studentdashboard/Studentcoinstatistic";
 
 const StudentGroup = () => {
   const [topStudent, setTopStudent] = useState([]);
 
   useEffect(() => {
     setConfig();
-    axios.get(url + "user/student/group/students", config)
-      .then(res => {
+    axios
+      .get(url + "user/student/group/students", config)
+      .then((res) => {
         setTopStudent(res.data.body);
       })
-      .catch(err => console.log("Boshqa backendchi topiyla iltimos 😭", err));
+      .catch((err) => console.log("Boshqa backendchi topiyla iltimos 😭", err));
   }, []);
 
   return (
@@ -20,19 +21,17 @@ const StudentGroup = () => {
       <div className="mb-10">
         <h2 className="text-3xl font-extrabold text-gray-900 mb-4">Exchange</h2>
       </div>
-      <div className='flex justify-around mb-10 px-10'>
-        <div className='w-5/12 text-5xl flex items-center h-64'>
-          MY group
-        </div>
-        <div className='w-5/12 shadow-xl up h-64'>
-          <Studentstatistic />
+      <div className="flex justify-around mb-10 px-10">
+        <div className="w-5/12 text-5xl flex items-center h-64">MY group</div>
+        <div className="w-5/12 h-max shadow-xl up h-64">
+          <Studentcoinstatistic />
         </div>
       </div>
       <div>
         {/* ... */}
         <table className="w-full rounded-3xl rounded-t-3xl all-shadow">
           <thead className="bg-gray-800 text-white rounded-s-2xl uppercase text-sm leading-normal">
-            <tr className='rounded-s-2xl'>
+            <tr className="rounded-s-2xl">
               <th className="py-3 px-6 text-left rounded-tl-3xl">No</th>
               <th className="py-3 px-6 text-left">Photo</th>
               <th className="py-3 px-6 text-left">Full name</th>
@@ -46,9 +45,15 @@ const StudentGroup = () => {
             {topStudent.length > 0 ? (
               topStudent.map((student, index) => (
                 <tr key={student.id} className="border-b border-gray-200 ">
-                  <td className="py-3 px-6 text-left whitespace-nowrap">{index + 1}</td>
+                  <td className="py-3 px-6 text-left whitespace-nowrap">
+                    {index + 1}
+                  </td>
                   <td className="py-3 px-6 text-left">
-                    <img src={student.imgurl} alt="Student" className="h-10 w-10 rounded-full" />
+                    <img
+                      src={student.imgurl}
+                      alt="Student"
+                      className="h-10 w-10 rounded-full"
+                    />
                   </td>
                   <td className="py-3 px-6 text-left">{student.fullName}</td>
                   <td className="py-3 px-6 text-center">{student.groupName}</td>
@@ -58,7 +63,7 @@ const StudentGroup = () => {
                     <input
                       type="checkbox"
                       className="form-checkbox h-5 w-5 text-blue-600 rounded"
-                      checked={`${student.active ? 'checked' : ''}`}
+                      checked={`${student.active ? "checked" : ""}`}
                       readOnly
                     />
                   </td>
@@ -66,7 +71,9 @@ const StudentGroup = () => {
               ))
             ) : (
               <tr className="border-b border-gray-200 ">
-                <td className="py-3 px-6 text-left whitespace-nowrap">Not found 😊</td>
+                <td className="py-3 px-6 text-left whitespace-nowrap">
+                  Not found 😊
+                </td>
               </tr>
             )}
           </tbody>
