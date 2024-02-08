bdc8940bbbdb8115184887ad3c9807238abde567
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-08 21:36:07 +0500

    dsi

--- src/teacher/components/test/table-and-card/TeacherTestCard.jsx
+++ src/teacher/components/test/table-and-card/TeacherTestCard.jsx
@@ -6,7 +6,7 @@ import { getFile } from '../../../../components/api/api';
 function TeacherTestCard({ allTestCard, getTestTable, setTableHeddin }) {
     return (
         <>
-            {allTestCard ?
+            {allTestCard.length !== 0 ?
                 allTestCard.map((item) => (
                     <div className="bg-white inline-block m-2 rounded-lg all-shadow hover:shadow-xl duration-300 overflow-hidden max-w-sm w-72">
                         <div className="flex justify-center items-center w-full h-44 overflow-hidden">
@@ -41,22 +41,10 @@ function TeacherTestCard({ allTestCard, getTestTable, setTableHeddin }) {
                 )) :
                 <div className="bg-white inline-block m-2 rounded-lg all-shadow hover:shadow-xl duration-300 overflow-hidden max-w-sm w-72">
                     <div className="flex justify-center items-center w-full h-44 overflow-hidden">
-                        <img src={rasm} alt="Profile" className="h-full w-full hover:scale-110 duration-300" />
+                        <img src={rasm} alt="Profile" className="h-full w-full object-cover hover:scale-110 duration-300" />
                     </div>
                     <div className="mt-3 text-center p-3 pt-0">
-                        <h2 className="font-bold text-lg">C name</h2>
-                        <span className="text-gray-800 font-bold opacity-90">coin</span>
-                        <div className="flex justify-between items-center py-5">
-                            <div className='flex items-center justify-start w-6/12'>
-                                <FontAwesomeIcon icon={faQuestionCircle} className="text-red-500 text-2xl mr-2" />
-                                <span>total test</span>
-                            </div>
-                            <div className="flex items-center justify-start w-6/12">
-                                <FontAwesomeIcon icon={faClock} className="text-red-500 text-2xl mr-2" />
-                                <span>category.workTime</span>
-                            </div>
-                        </div>
-                        <button className="btm mt-3">See</button>
+                        <h2 className="font-semibold tracking-wide opacity-70 text-lg">Test not found 😊</h2>
                     </div>
                 </div>
             }
