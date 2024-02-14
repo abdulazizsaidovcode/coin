ef0b4fc6b6cd43482eccec5b1dcaf53e23d37f49
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-14 14:18:10 +0500

    testni xam responsaveligi buldi

--- src/teacher/components/test/table-and-card/TeacherTestCard.jsx
+++ src/teacher/components/test/table-and-card/TeacherTestCard.jsx
@@ -6,49 +6,53 @@ import { getFile } from '../../../../components/api/api';
 function TeacherTestCard({ allTestCard, getTestTable, setTableHeddin }) {
     return (
         <>
-            {allTestCard ?
-                allTestCard.map((item) => (
+            <div className="w-full flex justify-around flex-wrap gap-8 md:gap-5 lg:gap-10 xl:gap-16">
+                {allTestCard ?
+                    allTestCard.map((item) => (
+                        <div className='bg-white rounded-xl all-shadow hover:shadow-lg hover:shadow-slate-500 
+                            duration-300 overflow-hidden w-full md:w-[48%] lg:w-1/4 xl:max-w-72 h-[360px] sm:h-[450px] md:h-[360px]'>
+                            <div className="w-full h-44 sm:h-64 md:h-44 overflow-hidden">
+                                <img
+                                    src={item.attachmentId === null
+                                        ? rasm
+                                        : getFile + item.attachmentId
+                                    }
+                                    alt="profile"
+                                    className="h-full w-full hover:scale-110 duration-300"
+                                />
+                            </div>
+                            <div className="mt-3 text-center p-3 pt-0">
+                                <h2 className="font-bold text-lg">{item.categoryName}</h2>
+                                <span className="text-gray-800 font-bold opacity-90">{item.allHour} all hour</span>
+                                <div className="flex justify-between items-center py-5">
+                                    <div className='flex items-center justify-start w-6/12'>
+                                        <FontAwesomeIcon icon={faQuestionCircle} className="text-red-500 text-2xl mr-2" />
+                                        <span>{item.countOfTest} test live</span>
+                                    </div>
+                                    <div className="flex items-center justify-start w-6/12">
+                                        <FontAwesomeIcon icon={faClock} className="text-red-500 text-2xl mr-2" />
+                                        <span>{item.allHour} hour {item.allMinute} min</span>
+                                    </div>
+                                </div>
+                                <button className="btm" onClick={() => {
+                                    getTestTable(item.categoryId);
+                                    sessionStorage.setItem('testCategoryId', item.categoryId);
+                                    setTableHeddin(true);
+                                }}>View tests</button>
+                            </div>
+                        </div>
+
+                    )) :
                     <div className="bg-white inline-block m-2 rounded-lg all-shadow hover:shadow-xl duration-300 overflow-hidden max-w-sm w-72">
                         <div className="flex justify-center items-center w-full h-44 overflow-hidden">
-                            <img
-                                src={item.attachmentId === null
-                                    ? rasm
-                                    : getFile + item.attachmentId
-                                }
-                                alt="profile"
-                                className="h-full w-full hover:scale-110 duration-300"
-                            />
+                            <img src={rasm} alt="Profile" className="h-full w-full object-cover hover:scale-110 duration-300" />
                         </div>
                         <div className="mt-3 text-center p-3 pt-0">
-                            <h2 className="font-bold text-lg">{item.categoryName}</h2>
-                            <span className="text-gray-800 font-bold opacity-90">{item.allHour} all hour</span>
-                            <div className="flex justify-between items-center py-5">
-                                <div className='flex items-center justify-start w-6/12'>
-                                    <FontAwesomeIcon icon={faQuestionCircle} className="text-red-500 text-2xl mr-2" />
-                                    <span>{item.countOfTest} test live</span>
-                                </div>
-                                <div className="flex items-center justify-start w-6/12">
-                                    <FontAwesomeIcon icon={faClock} className="text-red-500 text-2xl mr-2" />
-                                    <span>{item.allMinute} minute</span>
-                                </div>
-                            </div>
-                            <button className="btm" onClick={() => {
-                                getTestTable(item.categoryId);
-                                sessionStorage.setItem('testCategoryId', item.categoryId);
-                                setTableHeddin(true);
-                            }}>View tests</button>
+                            <h2 className="font-semibold tracking-wide opacity-70 text-lg">Test not found 😊</h2>
                         </div>
                     </div>
-                )) :
-                <div className="bg-white inline-block m-2 rounded-lg all-shadow hover:shadow-xl duration-300 overflow-hidden max-w-sm w-72">
-                    <div className="flex justify-center items-center w-full h-44 overflow-hidden">
-                        <img src={rasm} alt="Profile" className="h-full w-full object-cover hover:scale-110 duration-300" />
-                    </div>
-                    <div className="mt-3 text-center p-3 pt-0">
-                        <h2 className="font-semibold tracking-wide opacity-70 text-lg">Test not found 😊</h2>
-                    </div>
-                </div>
-            }
+                }
+            </div>
         </>
     );
 }
