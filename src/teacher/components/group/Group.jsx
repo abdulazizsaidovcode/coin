0a73fa8d19f79c5fd416eb0f1b93091f66b9198d
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-15 14:14:24 +0500

    ketti yana bitta

--- src/teacher/components/group/Group.jsx
+++ src/teacher/components/group/Group.jsx
@@ -4,7 +4,7 @@ import "../../../globalcss/style.css";
 
 function Groups() {
     return (
-        <div className="bg-gray-100 min-h-screen p-8 w-full">
+        <div className="bg-gray-100 min-h-screen px-8 pt-10 pb-20 w-full">
             <GroupsTable />
         </div>
     );
