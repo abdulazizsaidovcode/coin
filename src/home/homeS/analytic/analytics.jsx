5531732419c657ed3fb4d7517ca337633fbc022b
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-16 18:43:07 +0300

    addasd

--- src/home/homeS/analytic/analytics.jsx
+++ src/home/homeS/analytic/analytics.jsx
@@ -1,9 +1,11 @@
+import { Reading1 } from "../img/navIndex"
+
 const Analytics = () => {
     return (
         <div className="w-full">
             <div className="max-w-screen-xl mx-auto">
                 <div>
-                    
+                    <img src={Reading1} alt="reading" />
                 </div>
             </div>
         </div>
--- src/home/homeS/img/navIndex.js
+++ src/home/homeS/img/navIndex.js
@@ -8,6 +8,12 @@ import Frame from './Frame.svg'
 import Frame2 from './Frame (1).svg'
 import group from './Group.svg'
 import AboutCompany from './aboutcompany.svg'
+import Reading1 from './readin1.svg'
+import Reading2 from './reading2.svg.svg'
+import Reading3 from './reading.svg'
+import Reading4 from './readin4g.svg'
+import Reading5 from './reading5.svg.svg'
+
 
 export {
     coinIcon,
@@ -20,4 +26,9 @@ export {
     Frame2,
     group,
     AboutCompany,
+    Reading1,
+    Reading2,
+    Reading3,
+    Reading4,
+    Reading5
 }
