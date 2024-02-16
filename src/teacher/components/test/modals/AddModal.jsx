51c81ed6454be06d7821a2347b1d7eacfd8a7cf1
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-16 16:17:33 +0500

    test qushyabdi

--- src/teacher/components/test/modals/AddModal.jsx
+++ src/teacher/components/test/modals/AddModal.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { byId, config, url } from "../../../../components/api/api";
 import axios from "axios";
 import { toast } from "react-toastify";
 import LoadingBtn from "../../loadingBtn/LoadingBtn";
 
 const AddModal = (props) => {
-    const { toggleMenu, isMenuOpen, testCategorySub, getTestTable, getAllTestCard } = props;
+    const {
+        toggleMenu,
+        isMenuOpen,
+        categoryFather,
+        testCategorySub,
+        getTestTable,
+        getAllTestCard,
+        getTestCategoryChild
+    } = props;
 
     const testCategoryId = sessionStorage.getItem('testCategoryId');
     const [divCount, setDivCount] = useState(1);
     const [answers, setAnswers] = useState([{ answer: '', values: [] }]);
+    const [parameters, setParameters] = useState(['']);
     const [isLoading, setIsLoading] = useState(false);
 
     const handleInputChange = (index, field, value) => {
@@ -34,15 +43,29 @@ const AddModal = (props) => {
         }
     };
 
+    const handleInputChangeP = (index, value) => {
+        const updatedParameters = [...parameters];
+        updatedParameters[index] = value;
+        setParameters(updatedParameters);
+    };
+
+    const handleAddParameter = () => {
+        setParameters(prevParameters => [...prevParameters, '']);
+    };
+
+    const handleRemoveParameter = (index) => {
+        setParameters(prevParameters => prevParameters.filter((_, i) => i !== index));
+    };
+
     // add test
     const addTest = () => {
         setIsLoading(true);
         let addData = {
             id: 0,
             question: byId('question').value,
-            categoryId: byId('categorySelect').value,
+            categoryId: byId('categorySelectChild').value,
             processMinute: byId('teacherTime').value,
-            parameters: ['int a', 'int b'],
+            parameters: parameters,
             answer: answers,
             grade: byId('teacherCoin').value,
             advice: byId('advice').value,
@@ -104,11 +127,28 @@ const AddModal = (props) => {
                                                     placeholder="Enter Advice..."
                                                     rows="3" className="w-full p-2 mt-1 border rounded-md bg-slate-200 focus:bg-slate-100 focus:outline-0 duration-300"></textarea>
 
-                                                <label htmlFor="categorySelect" className="block text-sm font-medium text-gray-700 mt-4">
-                                                    Select category
+                                                <label htmlFor="categorySelectFather" className="block text-sm font-medium text-gray-700 mt-4">
+                                                    Select father category
                                                 </label>
-                                                <select id="categorySelect" className='mt-1 p-2 bg-slate-200 focus:bg-slate-100 focus:outline-0 duration-300 rounded-md w-full'>
-                                                    <option selected disabled>Select category</option>
+                                                <select
+                                                    id="categorySelectFather"
+                                                    onChange={(e) => {
+                                                        getTestCategoryChild(e.target.value);
+                                                    }} className='mt-1 p-2 bg-slate-200 focus:bg-slate-100 focus:outline-0 duration-300 rounded-md w-full'>
+                                                    <option selected disabled>Select father category</option>
+                                                    {categoryFather && categoryFather.map((item) =>
+                                                        <option value={item.id}>{item.name}</option>
+                                                    )}
+                                                </select>
+
+                                                <label htmlFor="categorySelectChild" className="block text-sm font-medium text-gray-700 mt-4">
+                                                    Select child category
+                                                </label>
+                                                <select
+                                                    disabled={testCategorySub.length !== 0 ? false : true}
+                                                    id="categorySelectChild"
+                                                    className='mt-1 p-2 bg-slate-200 focus:bg-slate-100 focus:outline-0 duration-300 rounded-md w-full'>
+                                                    <option selected disabled>Select child category</option>
                                                     {testCategorySub && testCategorySub.map((item) =>
                                                         <option value={item.id}>{item.name}</option>
                                                     )}
@@ -131,18 +171,33 @@ const AddModal = (props) => {
                                                     </div>
                                                 </div>
 
-                                                <label htmlFor="teacherParam" className="text-sm font-medium text-gray-700">
-                                                    Parametrs
-                                                </label>
-                                                <input
-                                                    id="teacherParam"
-                                                    placeholder="Enter parametr"
-                                                    className="mt-1 w-full rounded-md p-2 bg-slate-200 focus:bg-slate-100 focus:outline-0 duration-300" />
+                                                {parameters.map((param, index) => (
+                                                    <div key={index} className="w-full flex justify-between items-center gap-5">
+                                                        <div className="w-[90%]">
+                                                            <label htmlFor={`teacherParam${index}`} className="text-sm font-medium text-gray-700">
+                                                                Parameters
+                                                            </label>
+                                                            <input
+                                                                id={`teacherParam${index}`}
+                                                                placeholder="Enter parameter"
+                                                                className="mt-1 w-full rounded-md p-2 bg-slate-200 focus:bg-slate-100 focus:outline-0 duration-300"
+                                                                value={param}
+                                                                onChange={e => handleInputChangeP(index, e.target.value)}
+                                                            />
+                                                        </div>
+                                                        <div className="w-[10%] flex justify-end flex-wrap text-[1.5rem] leading-7 mt-5 font-bold">
+                                                            <span className="w-full text-center hover:cursor-pointer hover:text-slate-400 duration-300" onClick={handleAddParameter}>+</span>
+                                                            {parameters.length > 1 && (
+                                                                <span className="w-full text-center hover:cursor-pointer hover:text-slate-400 duration-300" onClick={() => handleRemoveParameter(index)}>-</span>
+                                                            )}
+                                                        </div>
+                                                    </div>
+                                                ))}
 
                                                 {/* plus inputs */}
                                                 <div className="flex justify-between items-center flex-wrap">
                                                     {[...Array(divCount)].map((_, index) => (
-                                                        <div key={index} className="flex justify-start w-[96%] mt-4">
+                                                        <div key={index} className="flex justify-start w-[90%] mt-4">
                                                             <div className="mr-1">
                                                                 <label htmlFor={`teacherAnswer${index}`} className="text-sm font-medium text-gray-700">
                                                                     Answer
@@ -169,15 +224,15 @@ const AddModal = (props) => {
                                                             </div>
                                                         </div>
                                                     ))}
-                                                    <div className="w-[4%] leading-5">
+                                                    <div className="w-[10%] leading-6">
                                                         <button
-                                                            className="font-bold text-black text-[1.5rem] mt-10 
-                                                            hover:text-slate-700 duration-300"
+                                                            className="font-bold text-black text-[1.5rem] mt-10 w-full
+                                                            hover:text-slate-400 duration-300"
                                                             onClick={handleAddDiv}
                                                         >+</button>
                                                         <button
-                                                            className="font-bold text-black text-[1.8rem]
-                                                            hover:text-slate-700 duration-300"
+                                                            className="font-bold text-black text-[1.8rem] w-full
+                                                            hover:text-slate-400 duration-300"
                                                             onClick={handleRemoveDiv}
                                                         >-</button>
                                                     </div>
@@ -209,4 +264,4 @@ const AddModal = (props) => {
     )
 }
 
-export default AddModal
+export default AddModal;
