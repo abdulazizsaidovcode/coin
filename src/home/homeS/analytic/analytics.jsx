d48c1f9abd114f0ac17f471e85001fc6165fb5f2
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-16 18:40:58 +0300

    awdawda

---
+++ src/home/homeS/analytic/analytics.jsx
@@ -0,0 +1,13 @@
+const Analytics = () => {
+    return (
+        <div className="w-full">
+            <div className="max-w-screen-xl mx-auto">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analytics
--- src/home/homeS/home.jsx
+++ src/home/homeS/home.jsx
@@ -3,6 +3,7 @@ import HomeNavbar from './NavbarSection/homeNavbar'
 import './home.css'
 import AboutSection from './homeSection/aboutSection'
 import About from './about/about'
+import Analytics from './analytic/analytics'
 
 const Home = () => {
 
@@ -11,6 +12,7 @@ const Home = () => {
       <div className='homeBody  w-[1430px] mx-auto px-5   '>
         <HomeNavbar />
         <AboutSection />
+        <Analytics/>
         <About />
       </div>
     </>
