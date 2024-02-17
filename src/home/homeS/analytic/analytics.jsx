3c49dadd45b36eac6ca1410dbc394fe1ee94f65b
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-17 14:16:04 +0300

    frascfascd

--- src/home/homeS/analytic/analytics.jsx
+++ src/home/homeS/analytic/analytics.jsx
@@ -1,34 +1,42 @@
-import { Reading1, Reading2, Reading3, Reading4, Reading5, Reading6 } from "../img/navIndex"
+import { Reading1, Reading2, Reading3, Reading4, alt, } from "../img/navIndex"
+import './analytic.css'
 
 const Analytics = () => {
+    const analytic = [
+        {
+            img: Reading1,
+            number: '15K+',
+            title: 'Number of students'
+        },
+        {
+            img: Reading2,
+            number: '75%',
+            title: 'Percentage of success'
+        },
+        {
+            img: Reading3,
+            number: '35',
+            title: 'Numbers of questions'
+        },
+        {
+            img: Reading4,
+            number: '25+',
+            title: 'Number of experts'
+        },
+    ]
     return (
         <div className="w-full">
-            <div className="max-w-screen-xl mx-auto flex flex-wrap">
-                <div>
-                    <img src={Reading1} alt="reading" />
-                    <h1>15K+</h1>
-                    <p>Number of students</p>
-                </div>
-                <div>
-                    <img src={Reading2} alt="reading" />
-                    <h1>15K+</h1>
-                    <p>Number of students</p>
-                </div>
-                <div>
-                    <img src={Reading3} alt="reading" />
-                    <h1>15K+</h1>
-                    <p>Number of students</p>
-                </div>
-                <div>
-                    <img src={Reading4} alt="reading" />
-                    <h1>15K+</h1>
-                    <p>Number of students</p>
-                </div>
-                <div>
-                    <img src={Reading6} alt="reading" />
-                    <h1>15K+</h1>
-                    <p>Number of students</p>
-                </div>
+            <h1 className="flex justify-center h-[3.5rem] mb-20 font-extrabold text-5xl analyticH1 ">Our succes <span></span> </h1>
+            <div className="max-w-screen-xl mx-auto gap-5 cards ">
+                {
+                    analytic.map((item) => (
+                        <div key={item} className=" card ">
+                            <img src={item.img} className="mb-5 mt-5" alt="reading" />
+                            <h1 className="text-white text-2xl font-bold">{item.number}</h1>
+                            <p className="text-white ">{item.title}</p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
