6e7764fcc33ea080effce4c6741cb0b4cac25af8
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-02-19 19:02:29 +0500

    wsxxwsx

--- src/student/studentnavbar/index.jsx
+++ src/student/studentnavbar/index.jsx
@@ -161,15 +161,18 @@ const StudentNavbar = () => {
                 >
                   Edit
                 </button>
+                <a href='/'>
+
                 <button
                   className="bg-red-500 text-white font-bold rounded-lg py-2.5 px-7 active:scale-90 duration-200"
                   onClick={() => {
-                    logOut();
+                    toggleMenu();
                     sessionStorage.clear();
                   }}
                 >
                   Log out
                 </button>
+                </a>
               </div>
             </div>
           </div>
