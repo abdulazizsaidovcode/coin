d16bcb43ca1fc19bb76c31e7219af613f495f08f
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-02-20 17:19:42 +0500

    e

--- src/student/studentnavbar/index.jsx
+++ src/student/studentnavbar/index.jsx
@@ -79,18 +79,7 @@ const StudentNavbar = () => {
       <Link to="/" id="logout"></Link>
       <div className="w-full ">
         <div className="flex justify-end bg-white py-3 px-8  z-50 w-full">
-          {/* Qidiruv maydoni */}
-          {/* <div className="flex items-center space-x-1 ">
-            <div class="relative">
-              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                </svg>
-              </div>
-              <input type="search" id="search" class="block w-full p-4 ps-10 text-sm  border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" />
-            </div>
-          </div> */}
-          {/* Foydalanuvchi profili va boshqa kontentlar uchun joy */}
+          
           <div className="relative left-0">
             <div className="flex items-center">
               <div className="relative mt-1">
