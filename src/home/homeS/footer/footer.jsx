564f8ed198aa95f41bbd925fb9d5f29f4edf6b28
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-20 16:49:23 +0300

    sasa

--- src/home/homeS/footer/footer.jsx
+++ src/home/homeS/footer/footer.jsx
@@ -70,6 +70,7 @@ const Footer = () => {
                     </a>
                 </div>
             </div>
+
             <div className="mx-6 py-10 text-center md:text-left">
                 <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
                     <div className="">
