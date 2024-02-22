342508a658c497355b86278622962ec59b15bfe8
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-02-22 12:11:50 +0300

    sasasasas

--- src/home/homeS/home.jsx
+++ src/home/homeS/home.jsx
@@ -8,8 +8,9 @@ import Footer from './footer/footer'
 import Contact from './Contact/contact'
 
 const Home = () => {
-  
+ 
   return (
+  
     <>
       <div className='homeBody  w-[1430px] mx-auto px-5'>
         <HomeNavbar />
