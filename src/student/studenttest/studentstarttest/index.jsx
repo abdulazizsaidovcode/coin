67a5a9c11780d3f140f6e6c968b6c9e08bf2bf3c
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-03-07 17:12:23 +0500

    somw thing changed

--- src/student/studenttest/studentstarttest/index.jsx
+++ src/student/studenttest/studentstarttest/index.jsx
@@ -15,9 +15,9 @@ function StudentStartTest() {
     useEffect(() => {
         setConfig();
         getOneTest(sessionStorage.getItem('testId'), setTest);
-        // return () => {
-        //     localStorage.removeItem('ts')
-        //  }
+        return () => {
+            localStorage.removeItem('ts')
+         }
     }, []);
 
     useEffect(() => {
@@ -71,7 +71,7 @@ function StudentStartTest() {
                         </svg>
                         <span className="sr-only">Loading...</span>
                     </div> : "Run"}</button>
-                    <span className="bg-white p-2 rounded-2xl text-lg font-mono text-gray-700">{time}:{second}</span>
+                    <span className="bg-gray-200  py-2 px-4 rounded-2xl text-lg font-mono text-gray-700">{time}:{second}</span>
                 </div>
                 <div className="flex space-x-4 mt-4 ">
                     <div className="flex-1">
