aa72e038315a6e5408c7aa6497f4732fcd174de2
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-03-08 16:46:55 +0500

    xalq class deb yozmanglar className yozish qiyinmi

--- src/admin/adminGroup/student/Students.jsx
+++ src/admin/adminGroup/student/Students.jsx
@@ -32,9 +32,9 @@ const GrStudents = () => {
         <div className="p-8 w-full bg-studentTableBg min-h-full">
             <div className=" mb-4 flex justify-between items-center">
                 <h2 className="text-4xl font-bold font-inika text-gray-900 mb-4">students</h2>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                     </div>
--- src/assits/loader.js
+++ src/assits/loader.js
@@ -3,23 +3,23 @@ import "./loader.css"
 
 function Loader() {
   return (
-    <div class="loader-circle-44 flex justify-center items-center">
-      <div class="bar-1"></div>
-      <div class="bar-2"></div>
-      <div class="bar-3"></div>
-      <div class="bar-4"></div>
-      <div class="bar-5"></div>
-      <div class="bar-6"></div>
-      <div class="bar-7"></div>
-      <div class="bar-8"></div>
-      <div class="bar-9"></div>
-      <div class="bar-10"></div>
-      <div class="bar-11"></div>
-      <div class="bar-12"></div>
-      <div class="bar-13"></div>
-      <div class="bar-14"></div>
-      <div class="bar-15"></div>
-      <div class="bar-16"></div>
+    <div className="loader-circle-44 flex justify-center items-center">
+      <div className="bar-1"></div>
+      <div className="bar-2"></div>
+      <div className="bar-3"></div>
+      <div className="bar-4"></div>
+      <div className="bar-5"></div>
+      <div className="bar-6"></div>
+      <div className="bar-7"></div>
+      <div className="bar-8"></div>
+      <div className="bar-9"></div>
+      <div className="bar-10"></div>
+      <div className="bar-11"></div>
+      <div className="bar-12"></div>
+      <div className="bar-13"></div>
+      <div className="bar-14"></div>
+      <div className="bar-15"></div>
+      <div className="bar-16"></div>
     </div>
   )
 }
